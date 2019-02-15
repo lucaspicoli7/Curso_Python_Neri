81311b323df50b5f55a9bac7adc96efb724df98f
Lucas Picoli <lucaspicolipy@gmail.com>
2019-02-15 14:38:04 -0200

    Meu Primeiro Commit

---
+++ 01-Operadores_Logicos_01.py
@@ -0,0 +1,11 @@
+print("<<< Operadores de Lógica >>>" + chr(10))
+print("Operador de Igualdade, Operador de Diferenciação, Operador de Comparação" + chr(10))
+
+nota  = 10
+media = 7
+
+print ("Nota é igual à Média.....:",nota == media)     # <-- Igualdade
+print ("Nota é Diferente da Média:",nota != media)     # <-- Diferenciação                 
+print ("Nota é Maior que a Média.:",nota >= media)     # <-- Comparação    
+print ("Passou?..................:",nota >= media)
+
